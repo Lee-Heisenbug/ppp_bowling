9b7d064fbd11de6c74a4de195838e8869d3297c4
Lee-Heisenbug <1070068427@qq.com>
2019-05-19 18:38:11 +0800

    Add more test case.

--- __test__/Game.test.js
+++ __test__/Game.test.js
@@ -71,3 +71,90 @@ test( 'test strike', () => {
     expect( g.score() ).toBe( 28 );
 
 } )
+
+test( 'test perfect game', () => {
+
+    for( let i = 0; i < 12; ++i ) {
+
+        g.add( 10 );
+
+    }
+
+    expect( g.getCurrentFrame() ).toBe( 11 );
+    expect( g.score() ).toBe( 300 );
+
+} )
+
+test( 'test end of array', () => {
+
+    for( let i = 0; i < 9; ++i ) {
+
+        g.add( 0 );
+        g.add( 0 );
+
+    }
+
+    g.add( 2 );
+    g.add( 8 );
+    g.add( 10 );
+
+    expect( g.score() ).toBe( 20 );
+
+} )
+
+test( 'test sample game', () => {
+
+    g.add( 1 );
+    g.add( 4 );
+    g.add( 4 );
+    g.add( 5 );
+    g.add( 6 );
+    g.add( 4 );
+    g.add( 5 );
+    g.add( 5 );
+    g.add( 10 );
+    g.add( 0 );
+    g.add( 1 );
+    g.add( 7 );
+    g.add( 3 );
+    g.add( 6 );
+    g.add( 4 );
+    g.add( 10 );
+    g.add( 2 );
+    g.add( 8 );
+    g.add( 6 );
+
+    expect( g.score() ).toBe( 133 );
+
+} )
+
+test( 'test heart break game', () => {
+
+    for( let i = 0; i < 11; ++i ) {
+
+        g.add( 10 );
+
+    }
+
+    g.add( 9 );
+    
+    expect( g.score() ).toBe( 299 );
+
+} )
+
+test( 'test tenth frame spare', () => {
+
+    for( let i = 0; i < 9; ++i ) {
+
+        g.add( 10 );
+
+    }
+
+    g.add( 9 );
+    g.add( 1 );
+
+    g.add( 1 );
+
+    expect( g.score() ).toBe( 270 );
+
+} )
