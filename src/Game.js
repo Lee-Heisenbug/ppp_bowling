62ff36208dc06f7eb8e2bc6a38d5761db49dab2f
李海丛 <haicong.li@goqo.com.cn>
2019-05-20 19:52:21 +0800

    Add class Scorer to calculate score.

--- src/Game.js
+++ src/Game.js
@@ -1,102 +1,13 @@
+import Scorer from './Scorer';
+
 class Game {
 
     constructor() {
 
         this._itsScore = 0;
-        this._itsThrows = [];
         this._itsCurrentFrame = 1;
         this._firstThrowInFrame = true;
-        this._firstBallInFrame = 0;
-
-    }
-
-    score() {
-
-        return this.scoreForFrame( this.getCurrentFrame() - 1 );
-
-    }
-
-    /**
-     * @param { number } theFrame 
-     */
-    scoreForFrame( theFrame ) {
-
-        let score = 0;
-        this._firstBallInFrame = 0;
-
-        for( let currentFrame = 1; currentFrame <= theFrame; ++currentFrame ) {
-
-            if( this._isStrike() ) {
-
-                score += 10 + this._next2ThrowsForStrike();
-
-                ++this._firstBallInFrame;
-
-            } else if( this._isSpare() ) {
-
-                score += 10 + this._nextThrowForSpare();
-
-                this._firstBallInFrame += 2;
-
-            } else {
-
-                score += this._twoThrowsInframe();
-
-                this._firstBallInFrame += 2;
-
-            }
-
-        }
-
-        return score;
-
-    }
-
-    _isStrike() {
-
-        return this._firstThrow() === 10;
-
-    }
-
-    _isSpare() {
-
-        return this._firstThrow() + this._nextThrow() === 10;
-
-    }
-
-    _next2ThrowsForStrike() {
-
-        return this._nextThrow() + this._next2thThrow();
-
-    }
-
-    _nextThrowForSpare() {
-
-        return this._next2thThrow();
-
-    }
-
-    _twoThrowsInframe() {
-
-        return this._firstThrow() + this._nextThrow();
-
-    }
-
-    _firstThrow() {
-
-        return this._itsThrows[ this._firstBallInFrame ];
-
-    }
-
-    _nextThrow() {
-
-        return this._itsThrows[ this._firstBallInFrame + 1 ];
-
-    }
-
-    _next2thThrow() {
-
-        return this._itsThrows[ this._firstBallInFrame + 2 ];
+        this._scorer = new Scorer();
 
     }
 
@@ -105,7 +16,7 @@ class Game {
      */
     add( pins ) {
 
-        this._itsThrows.push( pins );
+        this._scorer.addThrow( pins );
         this._itsScore += pins;
         this._ajustCurrentFrame( pins );
 
@@ -142,6 +53,21 @@ class Game {
     getCurrentFrame() {
 
         return this._itsCurrentFrame;
+
+    }
+
+    score() {
+
+        return this.scoreForFrame( this.getCurrentFrame() - 1 );
+
+    }
+
+    /**
+     * @param { number } theFrame 
+     */
+    scoreForFrame( theFrame ) {
+
+        return this._scorer.scoreForFrame( theFrame );
 
     }
 
---
+++ src/Scorer.js
@@ -0,0 +1,105 @@
+class Scorer {
+
+    constructor() {
+
+        this._itsThrows = [];
+        this._firstBallInFrame = 0;        
+
+    }
+
+    /**
+     * @param { number } pins 
+     */
+    addThrow( pins ) {
+
+        this._itsThrows.push( pins );
+
+    }
+
+    /**
+     * @param { number } theFrame 
+     */
+    scoreForFrame( theFrame ) {
+
+        let score = 0;
+        this._firstBallInFrame = 0;
+
+        for( let currentFrame = 1; currentFrame <= theFrame; ++currentFrame ) {
+
+            if( this._isStrike() ) {
+
+                score += 10 + this._next2ThrowsForStrike();
+
+                ++this._firstBallInFrame;
+
+            } else if( this._isSpare() ) {
+
+                score += 10 + this._nextThrowForSpare();
+
+                this._firstBallInFrame += 2;
+
+            } else {
+
+                score += this._twoThrowsInframe();
+
+                this._firstBallInFrame += 2;
+
+            }
+
+        }
+
+        return score;
+
+    }
+
+    _isStrike() {
+
+        return this._firstThrow() === 10;
+
+    }
+
+    _isSpare() {
+
+        return this._firstThrow() + this._nextThrow() === 10;
+
+    }
+
+    _next2ThrowsForStrike() {
+
+        return this._nextThrow() + this._next2thThrow();
+
+    }
+
+    _nextThrowForSpare() {
+
+        return this._next2thThrow();
+
+    }
+
+    _twoThrowsInframe() {
+
+        return this._firstThrow() + this._nextThrow();
+
+    }
+
+    _firstThrow() {
+
+        return this._itsThrows[ this._firstBallInFrame ];
+
+    }
+
+    _nextThrow() {
+
+        return this._itsThrows[ this._firstBallInFrame + 1 ];
+
+    }
+
+    _next2thThrow() {
+
+        return this._itsThrows[ this._firstBallInFrame + 2 ];
+
+    }
+
+}
+
+export default Scorer;
